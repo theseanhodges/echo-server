7e397416f542f2252b23e9bd94d12572550d89cd
Sean Hodges <sthodges@gmail.com>
2021-03-02 14:34:12 -0800

    Cleanup todo's

--- echo_client.py
+++ echo_client.py
@@ -5,18 +5,12 @@ import traceback
 
 def client(msg, log_buffer=sys.stderr):
     server_address = ('localhost', 10000)
-    # TODO: Replace the following line with your code which will instantiate
-    #       a TCP socket with IPv4 Addressing, call the socket you make 'sock'
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print('connecting to {0} port {1}'.format(*server_address), file=log_buffer)
     sock.connect(server_address)
-
-    # you can use this variable to accumulate the entire message received back
-    # from the server
+    
     received_message = ''
 
-    # this try/finally block exists purely to allow us to close the socket
-    # when we are finished with it
     try:
         print('sending "{0}"'.format(msg), file=log_buffer)
         sock.sendall(msg.encode())
